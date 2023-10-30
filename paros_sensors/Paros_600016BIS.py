3bcd9228ff65b8546cfd52f4421678e90f746d22
hakasapl <hakansaplakog@gmail.com>
2023-10-30 00:00:36 -0400

    fixed bugs in paros

--- paros_sensors/Paros_600016BIS.py
+++ paros_sensors/Paros_600016BIS.py
@@ -1,15 +1,20 @@
 import glob
 import serial
+import os
+import persistqueue
 from datetime import datetime
 
-class Sensor_6000_16B_IS:
-    def __main__(self, serial_num, fs, aa_cutoff):
+class Paros_600016BIS:
+    def __init__(self, serial_num, fs, aa_cutoff, buffer_on, buffer, log_on, logdir):
         self.serial_num = serial_num
         self.sensorPort = None
         self.waitFlag = False  # no response within timeout --> no barometer
 
+        self.fs = fs
+        self.aa_cutoff = aa_cutoff
+
         # Validation step
-        if fs < 2*fs:
+        if self.fs < 2*self.aa_cutoff:
             raise Exception(f"fs must be greater than 2x the AA cutoff on {self.serial_num}")
 
         # Get all USB TTYs on the device
@@ -32,13 +37,13 @@ class Sensor_6000_16B_IS:
             sensorPort.timeout = 0.2  # needs to be long enough to wake barometer and get response
             sensorPort.open()
 
-            sensorModelNumber = self.__sendCommand('*0100MN')
+            sensorModelNumber = self.__sendCommand('*0100MN', sensorPort)
             if "6000-16B-IS" not in sensorModelNumber:
                 continue
 
             baroSerialNumber = "BLANK"
             while not baroSerialNumber.isnumeric():
-                baroSerialNumber = self.__sendCommand('*0100SN')[3:]
+                baroSerialNumber = self.__sendCommand('*0100SN', sensorPort)[3:]
 
             if baroSerialNumber == self.serial_num:
                 self.sensorPort = sensorPort
@@ -47,11 +52,22 @@ class Sensor_6000_16B_IS:
         if self.sensorPort is None:
             raise Exception(f"6000-16B-IS not found with serial {self.serial_num}")
         
+        # Create log dir variables
+        self.log_on = log_on
+        self.buffer_on = buffer_on
+
+        if self.buffer_on:
+            self.buffer = buffer
+
+        if self.log_on:
+            self.logdir = os.path.join(logdir, self.serial_num)
+        
+        # Barometer exists so now we reset waitflag
         self.waitFlag = True
 
         # Configure settings on barometer
         fixedSettingsList = ['VR=Q1.03','XM=1','UN=2','MD=0','XN=0','TS=1','GE=1','TJ=0','TF=.00','TP=0','GT=1','GD=0']
-        configurableSettingsList = [f'TH={str(fs)},P4;>OK',f'IA={str(aa_cutoff)}']
+        configurableSettingsList = [f'TH={str(self.fs)},P4;>OK',f'IA={str(self.aa_cutoff)}']
 
         # check fixed barometer settings, quit if not OK
         for configSetting in fixedSettingsList:
@@ -79,35 +95,71 @@ class Sensor_6000_16B_IS:
 
         # set the serial port timeout for each barometer to be larger than the sample period
         baroSamplePeriod = 1/self.fs
-        self.baroPort.timeout = 1.5 * baroSamplePeriod
+        self.sensorPort.timeout = 1.5 * baroSamplePeriod
 
         # Start P4 sampling
         self.__sendCommand('*0100P4')
+
+    def samplingLoop(self):
+        while True:
+            # wait for line
+            binIn = self.sensorPort.readline()
+            strIn = binIn.decode()
+
+            if "\n" not in strIn:
+                # this line was returned after the timeout was reached
+                # meaning there is something wrong with the connection
+                self.stopSampling()
+                return False
+
+            in_parts = strIn.split(",")
+
+            cur_timestamp = datetime.strptime(in_parts[1].rstrip(), "%m/%d/%y %H:%M:%S.%f")
+            cur_value = in_parts[2].rstrip()
+
+            sys_timestamp = datetime.utcnow()
+
+            if self.buffer_on:
+                sample_dict = {
+                    "id": self.serial_num,
+                    "value": cur_value,
+                    "timestamp": cur_timestamp,
+                    "sys_time": sys_timestamp
+                }
+
+                self.buffer.put(sample_dict)
+
+            if self.log_on:
+                log_line = f"{self.serial_num},{sys_timestamp},{cur_timestamp},{cur_value}"
+
+                hour_time = cur_timestamp.replace(minute=0, second=0, microsecond=0)
+                log_file = os.path.join(self.log_dir, f"{hour_time.isoformat()}.csv")
+                with open(log_file, 'a') as output_log:
+                    output_log.write(log_line)
 
     def stopSampling(self):
         # send a command to stop P4 continuous sampling - any command will do
         self.__sendCommand('*0100SN')
 
-    def getSample(self):
-        binIn = self.baroPort.readline()
-        if not binIn:
-            return None
+    def closePort(self):
+        # Close serial connection
+        self.sensorPort.close()
 
-        strIn = binIn.decode()
-        in_parts = strIn.split(",")
-
-        cur_timestamp = datetime.strptime(in_parts[1].rstrip(), "%m/%d/%y %H:%M:%S.%f")
-        cur_value = in_parts[2].rstrip()
-
-        return cur_timestamp, cur_value
-
-    def __sendCommand(self, strOut):
+    def __sendCommand(self, strOut, port = None):
         strOut = strOut + '\r\n'
         binOut = strOut.encode()
-        self.baroPort.write(binOut)
+
+        if port is None:
+            self.sensorPort.write(binOut)
+        else:
+            port.write(binOut)
 
         while True:
-            binIn = self.baroPort.readline()
+            if port is None:
+                binIn = self.sensorPort.readline()
+            else:
+                binIn = port.readline()
+
             strIn = binIn.decode()
 
             if strIn:
---
+++ paros_sensors/__init__.py
@@ -0,0 +1,4 @@
+# paros_sensors/__init__.py
+
+# You can import modules and classes from your package files here.
+from .Paros_600016BIS import Paros_600016BIS
