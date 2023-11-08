4ee25bc5c2802efa8fe8a518e68d4441b669c0a8
hakasapl <hakansaplakog@gmail.com>
2023-11-08 02:03:07 -0500

    added anemometer, threading

--- reader.py
+++ reader.py
@@ -9,9 +9,10 @@ import influxdb_client
 from influxdb_client.client.exceptions import InfluxDBError
 from datetime import datetime
 import requests
+import signal
 
 class parosReader:
-    def __init__(self, config_file, secrets_file):
+    def __init__(self, config_file):
         # Get config from json
         if not os.path.isfile(config_file):
             raise Exception(f"File {config_file} does not exist")
@@ -41,6 +42,11 @@ class parosReader:
         # create sensors
         self.__createSensors()
 
+        # Set graceful shutdown methods
+        signal.signal(signal.SIGINT, self.gracefullShutdown)
+        signal.signal(signal.SIGTERM, self.gracefullShutdown)
+        self.shutting_down = False
+
         # vars
         self.shutdown = False
 
@@ -68,16 +74,32 @@ class parosReader:
                     )
 
                 self.sensor_list.append(cur_obj)
+            elif sensor_type == "Anemometer-1733":
+                cur_obj = paros_sensors.Adafruit_Anemometer1733(
+                    self.config["box_name"],
+                    sensor_dict["id"],
+                    sensor_dict["fs"],
+                    sensor_dict["input_pin"],
+                    sensor_dict["upload"],
+                    self.buffer,
+                    sensor_dict["log_locally"],
+                    self.config["datadir"]
+                )
+
+                self.sensor_list.append(cur_obj)
+
+    def startProcessing(self):
+        self.processing = True
 
     def bufferLoop(self):
         sendFailed = False
 
-        while True:
+        while self.processing:
             if self.shutdown:
                 break
 
             # block until item available in queue
-            cur_item = self.buffer.get()
+            cur_item = self.buffer.get(timeout=1)
 
             try:
                 self.influx_write_api.write(
@@ -106,6 +128,18 @@ class parosReader:
     def getSensors(self):
         return self.sensor_list
 
+    def gracefullShutdown(self, *args):
+        self.logEvent("Stopping ParosBox Program...")
+
+        self.shutting_down = True
+        self.processing = False
+
+        for sensor in self.sensor_list:
+            sensor.stopSampling()
+
+    def isShuttingDown(self):
+        return self.shutting_down
+
     def logEvent(self, str):
         output_str = f"[{self.config['box_name']}] {str}"
         self.__logMessage(output_str)
@@ -130,11 +164,11 @@ def main():
     # Main method
     hostname = socket.gethostname()
     config_file = f"config/{hostname}.json"
-    secrets_file = f"config/{hostname}-secrets.json"
 
     # Reader object for this box
-    reader = parosReader(config_file, secrets_file)
+    reader = parosReader(config_file)
 
+    # Log startup message
     reader.logEvent("Starting ParosBox Program...")
 
     # Launch Threads
@@ -142,55 +176,71 @@ def main():
 
     # Launch Sampling Threads
     sampling_futures = []
-    num_sampling_threads = len(sensor_list)
+    num_sampling_threads = len(sensor_list)  # one thread for each sensor
     sampling_threadpoolexecutor = concurrent.futures.ThreadPoolExecutor(max_workers=num_sampling_threads)
 
+    # store sensor object and future as a tuple so both can be referenced together
     for i in reader.getSensors():
-        i.startSampling()
-        sampling_futures.append((i, sampling_threadpoolexecutor.submit(i.samplingLoop)))
+        i.startSampling()  # trigger start sampling on the sensor
+        sampling_futures.append((i, sampling_threadpoolexecutor.submit(i.samplingLoop)))  # launch sampling thread
 
     # Launch Processing Thread
     processing_threadpoolexecutor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+    reader.startProcessing()
     processing_future = processing_threadpoolexecutor.submit(reader.bufferLoop)
 
-    # Create Cleaning Thread
+    # Launch Cleaning Thread
     cleaning_threadpoolexecutor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
     cleaning_future = cleaning_threadpoolexecutor.submit(reader.cleanBuffer)
 
-    clean_interval = 3600  # Every hour
-    cur_interval = 0
+    clean_interval = 3600  # Every hour the sqlite db will be cleaned
+    cur_interval = 0  # iterates up to clean_interval
 
-    try:
-        while True:
-            # Check if it's time to clean buffer
-            if not cleaning_future.running() and cur_interval >= clean_interval:
-                cleaning_future = cleaning_threadpoolexecutor.submit(reader.cleanBuffer)
-                cur_interval = 0
+    failCount = 0
+    fail_thresh = 8
+    fail_hour = datetime.utcnow().hour
+    while not reader.isShuttingDown() and failCount < fail_thresh:
+        # reset fail stats every hour
+        cur_hour = datetime.utcnow().hour
+        if cur_hour != fail_hour:
+            failCount = 0
+            fail_hour = cur_hour
 
-            # check if sampling threads are running
-            for future in sampling_futures:
-                if future[1].done() or future[1].cancelled():
-                    # found dead thread
-                    reader.logEvent(f"{future[0].getID()} sampling thread has crashed, restarting...: {future[1].result}")
-                    future[0].startSampling()
-                    future = (future[0], sampling_threadpoolexecutor.submit(future[0].samplingLoop))
+        # Check if it's time to clean buffer
+        if not cleaning_future.running() and cur_interval >= clean_interval:
+            cleaning_future = cleaning_threadpoolexecutor.submit(reader.cleanBuffer)
+            cur_interval = 0
 
-            # check if processing thread is running
-            if processing_future.done() or processing_future.cancelled():
-                reader.logEvent(f"Processing thread has crashed, restarting...: {processing_future.result}")
-                processing_future = processing_threadpoolexecutor.submit(reader.bufferLoop)
+        # check if sampling threads are running
+        for future in sampling_futures:
+            if future[1].done() or future[1].cancelled():
+                # found dead thread
+                reader.logEvent(f"{future[0].getID()} sampling thread has crashed, restarting...: {future[1].result}")
+                #print(f"{future[0].getID()} sampling thread has crashed, restarting...: {future[1].result}")
+                future[0].startSampling()
+                future = (future[0], sampling_threadpoolexecutor.submit(future[0].samplingLoop))
+                failCount += 1
 
-            cur_interval += 1
-            # Wait 1 second between main loop
-            time.sleep(1)
-    except (KeyboardInterrupt, SystemExit, Exception):
-        reader.logEvent("Stopping ParosBox Program...")
+        # check if processing thread is running
+        if processing_future.done() or processing_future.cancelled():
+            reader.logEvent(f"Processing thread has crashed, restarting...: {processing_future.result}")
+            #print(f"{future[0].getID()} sampling thread has crashed, restarting...: {future[1].result}")
+            reader.startProcessing()
+            processing_future = processing_threadpoolexecutor.submit(reader.bufferLoop)
+            failCount += 1
 
-        for sensor in sensor_list:
-            sensor.stopSampling()
+        cur_interval += 1
+        # Wait 1 second between main loop
+        time.sleep(1)
 
-        sampling_threadpoolexecutor.shutdown(wait=False)
-        processing_threadpoolexecutor.shutdown(wait=False)
+    if failCount >= fail_thresh:
+        reader.logEvent(f"Shutting down threads due to > {str(fail_thresh)} thread crashes")
+        reader.gracefullShutdown()
+
+    # shutdown threads
+    processing_threadpoolexecutor.shutdown()
+    sampling_threadpoolexecutor.shutdown()
+    cleaning_threadpoolexecutor.shutdown()
 
 if __name__ == "__main__":
     main()
